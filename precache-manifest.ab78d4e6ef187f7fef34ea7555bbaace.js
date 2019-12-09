a3b14e0c44d0e1dc961b7911288925e5b15bdd2a
karolkrzych <karolkrzych@gmail.com>
2019-12-09 21:30:22 +0100

    Updates

---
+++ precache-manifest.ab78d4e6ef187f7fef34ea7555bbaace.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c76ea203f0aa7facefb13bb7e690cf",
+    "url": "/event-manager/index.html"
+  },
+  {
+    "revision": "c99d91558dad9c0cbdba",
+    "url": "/event-manager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "11e02735b89b4fc5f468",
+    "url": "/event-manager/static/css/main.4ce43f6c.chunk.css"
+  },
+  {
+    "revision": "c99d91558dad9c0cbdba",
+    "url": "/event-manager/static/js/2.1bf052ef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/event-manager/static/js/2.1bf052ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11e02735b89b4fc5f468",
+    "url": "/event-manager/static/js/main.012ed396.chunk.js"
+  },
+  {
+    "revision": "4304d6a1d374e1065923",
+    "url": "/event-manager/static/js/runtime-main.63d440a9.js"
+  }
+]);
